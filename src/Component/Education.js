5db61eeda311c21fa108cf7296bf3046984c7e14
Anuranjan-Shrivastava <s.anuranjan.shrivastava@gmail.com>
2022-01-27 06:31:47 +0530

    Improved About

--- src/Component/Education.js
+++ src/Component/Education.js
@@ -2,9 +2,55 @@ import React, { Component } from 'react';
 import '../Css/Education.css' ;
 
 class Education extends Component {
+    constructor(){
+        super() ;
+        this.state = {
+            level : 0  
+        }
+    }
+
+    moveTraveller = (dir) => {
+        var truck = document.getElementsByClassName('traveller')[0] ;
+        var post = truck.getBoundingClientRect() ;
+        var road = document.getElementsByClassName('education1')[0] ;
+        var posr = road.getBoundingClientRect() ;
+        console.log(posr) ;
+       
+        if(dir === "r"){
+            if(this.state.level === 2)return ;
+            let w = posr.right - posr.left ;
+            let wt = post.width ;  
+            if(this.state.level === 0){
+                truck.style.left = `${w/2-wt/2}px` ;
+            }else{
+                truck.style.left = `${w-wt}px` ;
+            }
+            
+            this.setState((prevState) => {
+                return {
+                    level : prevState.level + 1 
+                }
+            })
+           
+        }else {
+            if(this.state.level === 0)return ;
+            let w = posr.right - posr.left ;
+            let wt = post.width ;  
+            if(this.state.level === 2){
+                truck.style.left = `${w/2-wt/2}px` ;
+            }else{
+                truck.style.left = `${0}px` ;
+            }
+            this.setState((prevState) => {
+                return {
+                    level : prevState.level - 1 
+                }
+            })
+        }
+    }
     render() {
         return (
-            <div class="education">
+            <div className="education">
                 <div class="circle1">
                     <div class="circle1-detail">
                         <div>Bachelor of Engineering(CSE)</div>
@@ -25,6 +71,15 @@ class Education extends Component {
                         <div>CARE Public School,Bilaspur</div>
                         <div>2014-2015, Score : 8.4</div>
                     </div> 
+                </div>
+                <div className='education1'>
+                    <div className="traveller"><i class="fas fa-truck"></i></div>
+                </div>
+                <div className="driver">
+                    <i class="fas fa-arrow-circle-left"
+                      onClick={() => this.moveTraveller("l")}></i>
+                    <i class="fas fa-arrow-circle-right"
+                      onClick={() => this.moveTraveller("r")}></i>  
                 </div>
             </div>
         );
--- src/Component/TAbout.js
+++ src/Component/TAbout.js
@@ -1,12 +1,120 @@
 import React, { Component } from 'react';
 import '../Css/TAbout.css' ;
+import def from '../Css/defaultB.jpg' ;
+import deft from '../Css/defaultT.jpg' ;
+import b1 from '../Css/1.jpg' ;
+import b2 from '../Css/2.png' ;
+import b3 from '../Css/3.jpg' ;
+import b4 from '../Css/4.jpg' ;
+import b5 from '../Css/5.jpg' ;
+import b6 from '../Css/6.jpg' ;
+import t1 from '../Css/t1.png' ;
+import t2 from '../Css/t2.jpg' ;
+import t3 from '../Css/t3.png' ;
+import t4 from '../Css/t4.jpg' ;
+import t5 from '../Css/t5.jpg' ;
+import t6 from '../Css/t6.png' ;
+import to2 from '../Css/to2.jpg' ;
+import to1 from '../Css/to1.jpg' ;
 
 class TAbout extends Component {
+    constructor() {
+        super() ;
+        this.state = {
+            toggle : true  , 
+            arr : [b1,b2,b3,b4,b5,b6] , 
+            arrT : [t1 , t2 , t3 , t4 , t5 , t6] ,
+            slider : null 
+        }
+    }
+    toggle = () => {
+        if(this.state.toggle === true){
+            this.setState({
+                toggle : false 
+            })
+            let toggleButton = document.getElementsByClassName('togglebutton')[0] ;
+            toggleButton.style.left = "50%" ;
+            let toggler = document.getElementsByClassName('toggler')[0] ;
+            toggler.style.backgroundImage = `url(${to2})`;
+        }else{
+            this.setState({
+                toggle : true  
+            })
+            let toggleButton = document.getElementsByClassName('togglebutton')[0] ;
+            toggleButton.style.left = "0%" ;
+            let toggler = document.getElementsByClassName('toggler')[0] ;
+            toggler.style.backgroundImage = `url(${to1})`;
+        }
+    }
+    startSlideShow = async (field) => {
+       
+        if(field === "books"){
+            let frame = document.getElementsByClassName('tabout-extra')[0] ;
+            let photo = frame.children[0] ;
+            await this.setState({
+                slider : this.sliderfunction(photo , "books")
+            })
+        }
+        else{
+            let frame = document.getElementsByClassName('tabout-extra-tech')[0] ;
+            let photo = frame.children[0] ;
+            await this.setState({
+                slider : this.sliderfunction(photo ,"tech")
+            })
+        }
+               
+    }
+
+    stopSlideShow = async (field) => {
+        clearInterval(this.state.slider) ;
+            await this.setState({
+                slider : null 
+            
+        }) ; 
+        if(field === "books"){
+            let frame = document.getElementsByClassName('tabout-extra')[0] ;
+            let photo = frame.children[0] ;
+            photo.setAttribute('src' , def) ;
+        }
+        else{
+            let frame = document.getElementsByClassName('tabout-extra-tech')[0] ;
+            let photo = frame.children[0] ;
+            photo.setAttribute('src' , deft) ;
+        }
+       
+    }
+
+    sliderfunction(photo , field) {
+        if(field === "books"){
+                var books = this.state.arr ;
+                let index = 0 ; 
+                return setInterval(function(){
+                    if(index === 6){
+                        index = 0 ; 
+                    }
+                    var image = books[index] ;
+                    photo.setAttribute('src',image) ;
+                    index++ ; 
+            },500) ;
+        }else {
+            var tech = this.state.arrT ;
+            let index = 0 ; 
+            return setInterval(function(){
+                if(index === 6){
+                    index = 0 ; 
+                }
+                var image = tech[index] ;
+                photo.setAttribute('src',image) ;
+                index++ ; 
+        },500) ;
+        } 
+    }
     render() {
+        const { toggle } = this.state ; 
         return (
             <div className='tabout'>
                <div className='tabout-content'>
-                    <div className="tabout-detail">
+                   { toggle && <div className="tabout-detail">
                         <div class="tabout-content-helper-1"></div>
                         <div class="tabout-content-heading">
                                     <span class="about-content-heading-letter">A</span>
@@ -36,10 +144,23 @@ class TAbout extends Component {
                         <div class="tabout-content-helper-2">
 
                         </div>
-                    </div>
-                    <div className="tabout-picture">
-
-                    </div>
+                    </div>}
+                    {!toggle && <div className="tabout-picture">
+                            <div className="tabout-extra" 
+                                onMouseEnter = {() => this.startSlideShow("books")}
+                                onMouseOut = {() => this.stopSlideShow("books")}>
+                                <img alt="books" src={def}/>
+                            </div>
+                            <div className="tabout-extra-tech"
+                                onMouseEnter = {() => this.startSlideShow("tech")}
+                                onMouseOut = {() => this.stopSlideShow("tech")}>
+                                <img alt="books" src={deft}/>
+                            </div> 
+                    </div>}
+                    <div className='toggler'
+                         onClick={() => this.toggle()}>
+                             <div className='togglebutton'></div>
+                         </div>
                </div>
             </div>
         );
